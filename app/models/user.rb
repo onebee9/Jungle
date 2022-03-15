6de1c4e1b82ac50d1c3d01c6b52923e4d520ec77
ibifuro odu <oduibifuro@gmail.com>
2022-03-15 14:44:22 -0400

    set up feature specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,15 +2,17 @@ class User < ActiveRecord::Base
 
   has_secure_password
   
+  validates :email, :uniqueness => { :case_sensitive => false }
+  validates :email, presence: true
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true, length: { minimum: 6 }
+
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :email, presence: true
-  validates_uniqueness_of :email, :case_sensitive => false
-  validates :password, confirmation:true, length: { minimum: 8 }
-  validates :password_confirmation, presence: true
 
   def self.authenticate_with_credentials(email, password)
-    User.find_by_email(email.strip.downcase).try(:authenticate, password)
+    user = where('LOWER(email) = ?', email.downcase.delete(' '))[0]
+    user && user.authenticate(password)
   end
 
 end
